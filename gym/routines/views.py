214383d99b29a64a2f2a0bab719c71878b386331
abunuwas <joseharoperalta@gmail.com>
2017-10-28 19:45:35 +0100

    Add routintes views

--- gym/routines/views.py
+++ gym/routines/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Set, Exercise
+from .serializers import ExerciseSerializer, SetSerializer, UserSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class SetViewSet(viewsets.ModelViewSet):
+    queryset = Set.objects.all()
+    serializer_class = SetSerializer
+
+
+class ExerciseViewSet(viewsets.ModelViewSet):
+    queryset = Exercise.objects.all()
+    serializer_class = ExerciseSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
