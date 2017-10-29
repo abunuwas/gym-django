942abef3a033cac65f01083cc6fe9b12792bc1ce
abunuwas <joseharoperalta@gmail.com>
2017-10-29 10:14:54 +0000

    Add authentication urls

--- gym/gym/urls.py
+++ gym/gym/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework_jwt.views import obtain_jwt_token
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^routines/', include('routines.urls')),
+    #url(r'^accounts/', include('accounts.urls'), name='accounts'),
+    url(r'^routines/', include('routines.urls'), name='routines'),
+    url(r'^api-auth/', include('rest_framework.urls')),
+    url(r'^api-token-auth/', obtain_jwt_token, name='api_auth'),
 ]
